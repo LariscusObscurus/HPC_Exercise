090e83448b4ae303c22a123f99f730c4cd6d9a53
Leonhardt Schwarz <Leonhardt.Schwarz@zuehlke.com>
2018-05-07 19:27:03 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include "CL/cl.h"
+#include <vector>
+#include <iostream>
+#include <string>
+
+std::string GetPlatformName(cl_platform_id id)
+{
+	size_t size = 0;
+	clGetPlatformInfo(id, CL_PLATFORM_NAME, 0, nullptr, &size);
+
+	std::string result;
+	result.resize(size);
+	clGetPlatformInfo(id, CL_PLATFORM_NAME, size,
+		const_cast<char*> (result.data()), nullptr);
+
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	cl_uint platformIdCount = 0;
+	clGetPlatformIDs(0, nullptr, &platformIdCount);
+
+	if (platformIdCount == 0) {
+		std::cerr << "No OpenCL platform found" << std::endl;
+		return 1;
+	} else {
+		std::cout << "Found " << platformIdCount << " platform(s)" << std::endl;
+	}
+
+	std::vector<cl_platform_id> platformIds(platformIdCount);
+	clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
+
+	for (cl_uint i = 0; i < platformIdCount; ++i) {
+		std::cout << "\t (" << (i + 1) << ") : " << GetPlatformName(platformIds[i]) << std::endl;
+	}
+
+	std::getchar();
+	return 0;
+}

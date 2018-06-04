99b4ba77e3e21d6075ad971e2cb334a95fdeab50
leonhardt <lariscusObscurus@gmail.com>
2018-06-04 03:03:08 +0200

    array_size not range

--- src/exercise2.cpp
+++ src/exercise2.cpp
@@ -81,23 +81,22 @@ void gpu_prefixsum(cl::Context& context, cl::CommandQueue& queue, cl::Kernel& ke
     event.wait();
 }
 
-const auto group_size = 33;
+const auto group_size = 256;
 
 size_t round_for_block(size_t val) {
-        size_t res = val;
+    auto res = val;
 
-        if (val % group_size != 0) {
-            res += group_size- val % group_size;
-        }
+    if (val % group_size != 0) {
+        res += group_size - val % group_size;
+    }
 
-        return res;
+    return res;
 }
 
 void gpu_workefficient_prefixsum(cl::Context& context, cl::CommandQueue& queue, cl::Kernel& kernel, std::vector<int>& input, std::vector<int>& output, const opencl_manager& manager)
 {
     const auto input_buffer_size = input.size() * sizeof(int);
     const auto output_buffer_size = output.size() * sizeof(int);
-    const int local_size = sizeof(int) * group_size;
 
     const auto input_buffer = cl::Buffer(context, CL_MEM_READ_ONLY, input_buffer_size);
     const auto output_buffer = cl::Buffer(context, CL_MEM_READ_WRITE, output_buffer_size);
@@ -105,21 +104,23 @@ void gpu_workefficient_prefixsum(cl::Context& context, cl::CommandQueue& queue, 
     auto result = queue.enqueueWriteBuffer(input_buffer, CL_TRUE, 0, input_buffer_size, input.data());
     result = queue.finish();
 
-    std::function<void(const cl::Buffer&, const cl::Buffer&, std::size_t)> fun;
+    std::function<void(const cl::Buffer&, const cl::Buffer&, std::size_t)> scan;
 
-    fun = [&context, &queue, &kernel, &manager, local_size, &fun](const cl::Buffer& input_buffer, const cl::Buffer& output_buffer, const std::size_t array_size)
+    scan = [&context, &queue, &kernel, &manager, &scan](const cl::Buffer& input_buffer, const cl::Buffer& output_buffer, const std::size_t array_size)
     {
         auto range = round_for_block(array_size);
         const auto group_count = range / group_size;
+        constexpr int local_size = sizeof(int) * group_size;
 
         auto group_sums = cl::Buffer(context, CL_MEM_READ_WRITE, sizeof(int) * group_count);
+
         if (array_size <= group_size)
         {
             kernel.setArg(0, input_buffer);
             kernel.setArg(1, output_buffer);
             kernel.setArg(2, group_sums);
             kernel.setArg(3, cl::LocalSpaceArg(cl::Local(local_size)));
-            kernel.setArg(4, (int)range);
+            kernel.setArg(4, (int)array_size);
 
             auto event = cl::Event{};
             const auto rv = queue.enqueueNDRangeKernel(kernel, cl::NDRange(0), cl::NDRange(range), cl::NDRange(group_size), nullptr, &event);
@@ -138,7 +139,7 @@ void gpu_workefficient_prefixsum(cl::Context& context, cl::CommandQueue& queue, 
             kernel.setArg(1, scan_output_buffer);
             kernel.setArg(2, group_sums);
             kernel.setArg(3, cl::LocalSpaceArg(cl::Local(local_size)));
-            kernel.setArg(4, (int)range);
+            kernel.setArg(4, (int)array_size);
 
             auto event = cl::Event{};
 
@@ -149,7 +150,7 @@ void gpu_workefficient_prefixsum(cl::Context& context, cl::CommandQueue& queue, 
             }
             auto result = event.wait();
 
-            fun(group_sums, add_buffer, group_count);
+            scan(group_sums, add_buffer, group_count);
 
             //ADD
             auto add_kernel = manager.get_kernel("add_groups");
@@ -168,7 +169,7 @@ void gpu_workefficient_prefixsum(cl::Context& context, cl::CommandQueue& queue, 
         }
     };
 
-    fun(input_buffer, output_buffer, input.size());
+    scan(input_buffer, output_buffer, input.size());
 
     auto event = cl::Event{};
     result = queue.enqueueReadBuffer(output_buffer, CL_TRUE, 0, output_buffer_size, &output[0], nullptr, &event);
